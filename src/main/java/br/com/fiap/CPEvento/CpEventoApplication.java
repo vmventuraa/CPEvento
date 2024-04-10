ac4b7f4c361881a3851bc585381ef63004c4411e
Victor Ventura <vmventura025@gmail.com>
2024-04-10 12:42:11 -0300

    Correção do dropdown de cidades

--- src/main/java/br/com/fiap/CPEvento/CpEventoApplication.java
+++ src/main/java/br/com/fiap/CPEvento/CpEventoApplication.java
@@ -3,6 +3,12 @@ package br.com.fiap.CPEvento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* Integrantes
+* Victor Moura Ventura RM:93509
+* Pedro de Sousa Dutra RM 96167
+* */
+
 @SpringBootApplication
 public class CpEventoApplication {
 
